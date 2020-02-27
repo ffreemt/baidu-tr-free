8b242b9e378391d3416e00cbe707fa4cc63beba0
ffreemt <yucongo+fmt@gmail.com>
2020-02-27 23:16:04 +0800

    :up: updated to 0.0.8

--- bdtr/__init__.py
+++ bdtr/__init__.py
@@ -4,6 +4,6 @@ from .js2py_sign import js2py_sign
 
 # version__ = '0.0.4'
 # version__ = '0.0.5'  # 2019-11-21
-# version__ = '0.0.7'  # 2020-02-12
+# version = '0.0.7'  # 2020-02-12
 __version__ = '0.0.8'  # 2020-02-28
 VERSION = tuple(__version__.split('.'))
